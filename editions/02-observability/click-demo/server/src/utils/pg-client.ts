ba340bf5a74e053a786aa8d1c502b26084ea50ec
Elmer Bulthuis <elmerbulthuis@gmail.com>
2024-04-23 20:11:05 +0200

    comments

--- editions/02-observability/click-demo/server/src/utils/pg-client.ts
+++ editions/02-observability/click-demo/server/src/utils/pg-client.ts
@@ -4,6 +4,12 @@ import { usePgPool } from "./pg-pool.js";
 
 const storage = new AsyncLocalStorage<pg.PoolClient>();
 
+/**
+ * Executes the provided job function with a PostgreSQL client from the AsyncLocalStorage.
+ *
+ * @param {Function} job - A function that takes a PostgreSQL client and returns a Promise.
+ * @returns {Promise<T>} A Promise that resolves to the result of the provided job function.
+ */
 export async function withPgClient<T>(
   job: (client: pg.PoolClient) => Promise<T>
 ): Promise<T> {
@@ -19,6 +25,13 @@ export async function withPgClient<T>(
   }
 }
 
+/**
+ * Retrieves the current PostgreSQL client from the AsyncLocalStorage.
+ *
+ * @returns {pg.PoolClient} The current PostgreSQL client.
+ *
+ * @throws {TypeError} If no PostgreSQL client is found in the context.
+ */
 export function usePgClient(): pg.PoolClient {
   const database = storage.getStore();
   if (database == null) {
--- editions/02-observability/click-demo/server/src/utils/pg-pool.ts
+++ editions/02-observability/click-demo/server/src/utils/pg-pool.ts
@@ -1,12 +1,25 @@
 import { AsyncLocalStorage } from "async_hooks";
 import pg from "pg";
 
+/**
+ * A configuration interface for a PostgreSQL pool.
+ */
 export interface PgPoolConfiguration {
+  /**
+   * The PostgreSQL connection URI.
+   */
   pgUri: string;
 }
 
 const storage = new AsyncLocalStorage<pg.Pool>();
 
+/**
+ * A function that runs a job within the context of a PostgreSQL pool.
+ *
+ * @param configuration - The configuration for the PostgreSQL pool.
+ * @param job - The function to be executed within the context of the pool.
+ * @returns The result of the job.
+ */
 export async function withPgPool<T>(
   configuration: PgPoolConfiguration,
   job: (pool: pg.Pool) => Promise<T>
@@ -22,6 +35,12 @@ export async function withPgPool<T>(
   }
 }
 
+/**
+ * A function that retrieves the current PostgreSQL pool from the global storage.
+ *
+ * @returns The current PostgreSQL pool.
+ * @throws {TypeError} If no PostgreSQL pool is found in the context.
+ */
 export function usePgPool(): pg.Pool {
   const database = storage.getStore();
   if (database == null) {
